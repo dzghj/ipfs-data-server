2cdd8417e3c5cbfd759b1cd4de4c621bc2dc7e99
Zhigang deng <deng_zg@hotmail.com>
2026-02-13 17:54:08 -0800

    update db4

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import dotenv from "dotenv";
 import authRoutes, { auth } from "./auth.js";
 import { sequelize, FileRecord } from "./db.js";
 import { secureUpload } from "./secure-share/index.js";
+import { ipfs } from "./secure-share/ipfs-client.js";
+
 import fetch from "node-fetch"; // only if Node <20
 
 
