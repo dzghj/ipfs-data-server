7198e99d4fd1d8a9c7a3b23d08b111cd45f5404d
Zhigang deng <deng_zg@hotmail.com>
2026-03-30 14:30:06 -0700

    add AI Part4

--- auth.js
+++ auth.js
@@ -225,7 +225,7 @@ router.post("/login", async (req, res) => {
 
     res.json({
       token,
-      user: { id: user.id, email: user.email ,riskScore: data.riskScore,riskAnalysis: data.riskAnalysis}
+      user: { id: user.id, email: user.email ,riskScore: user.riskScore,riskAnalysis: user.riskAnalysis}
     });
 
   } catch (err) {
